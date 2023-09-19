d62d9fba0982ac73032454914825910de253d2a0
ruthyrodriguez <ruthcita03031989@gmail.com>
2023-09-19 15:54:29 -0400

    Cambio en proveedor

--- vistas/proveedor.php
+++ vistas/proveedor.php
@@ -58,9 +58,9 @@ if ($_SESSION['compras']==1) {
      <div class="form-group col-lg-6 col-md-6 col-xs-12">
       <label for="">Tipo Dcumento</label>
      <select class="form-control select-picker" name="tipo_documento" id="tipo_documento" required>
-       <option value="DNI">DNI</option>
-       <option value="RUC">RUC</option>
-       <option value="CEDULA">CEDULA</option>
+       <option value="DNI">CI</option>
+       <option value="RUC">RUN</option>
+       <option value="CEDULA">LICENCIA</option>
      </select>
     </div>
      <div class="form-group col-lg-6 col-md-6 col-xs-12">
