d2ac883a2f0bc2043e8b4ffd56b0148635cbc38e
ruthyrodriguez <ruthcita03031989@gmail.com>
2023-09-19 15:43:14 -0400

    Cambio en el header

--- vistas/header.php
+++ vistas/header.php
@@ -8,7 +8,7 @@ if (strlen(session_id())<1)
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>sistema ventas y almacen</title>
+  <title>Farmacia "Sit Lux"</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -36,9 +36,9 @@ if (strlen(session_id())<1)
     <!-- Logo -->
     <a href="escritorio.php" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>VENTAS</b> ALMACEN </span>
+      <span class="logo-mini"><b>FARMACIA</b> VENTAS </span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>VENTAS</b> ALMACEN</span>
+      <span class="logo-lg"><b>FARMACIA</b> VENTAS</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
